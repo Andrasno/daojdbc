dfb2d7ad0b527af60d658616a336da3a23fa02d4
Andre Torres <andrasno@gmail.com>
2019-12-11 21:59:53 -0300

    findById implementation

--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -10,59 +10,106 @@ public class Seller implements Serializable {
 	private String name;
 	private String email;
 	private Date birthDate;
+	private Double baseSalary;
 	private Department department;
 	
 	public Seller() {
 		
 	}
 
-	public Seller(Integer id, String name, String email, Date birthDate, Department department) {
+	
+
+	public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary, Department department) {
+		super();
 		this.id = id;
 		this.name = name;
 		this.email = email;
 		this.birthDate = birthDate;
+		this.baseSalary = baseSalary;
 		this.department = department;
 	}
+
+
+
+	
 
 	public Integer getId() {
 		return id;
 	}
 
+
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
+
 
 	public String getName() {
 		return name;
 	}
 
+
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
+
 
 	public String getEmail() {
 		return email;
 	}
 
+
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
+
 
 	public Date getBirthDate() {
 		return birthDate;
 	}
 
+
+
 	public void setBirthDate(Date birthDate) {
 		this.birthDate = birthDate;
 	}
+
+
+
+	public Double getBaseSalary() {
+		return baseSalary;
+	}
+
+
+
+	public void setBaseSalary(Double baseSalary) {
+		this.baseSalary = baseSalary;
+	}
+
+
 
 	public Department getDepartment() {
 		return department;
 	}
 
+
+
 	public void setDepartment(Department department) {
 		this.department = department;
 	}
+
+
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+
 
 	@Override
 	public int hashCode() {
@@ -89,11 +136,15 @@ public class Seller implements Serializable {
 		return true;
 	}
 
+
+
 	@Override
 	public String toString() {
-		return "Seller [id=" + id + ", name=" + name + ", email=" + email + ", birthDate=" + birthDate + ", department="
-				+ department + "]";
+		return "Seller [id=" + id + ", name=" + name + ", email=" + email + ", birthDate=" + birthDate + ", baseSalary="
+				+ baseSalary + ", department=" + department + "]";
 	}
+
+	
 	
 	
 }
